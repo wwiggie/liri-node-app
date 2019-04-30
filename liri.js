28e051a8b7262620bce133d02704313f9a9a665a
Wendy Wigg <wiggwendy@gmail.com>
2019-04-30 15:31:30 -0700

    successful OMDB responses

--- liri.js
+++ liri.js
@@ -12,21 +12,72 @@ var spotify = new Spotify({
     secret: keys.spotify.secret
 });
 
-// change "I saw the sign" to process.argv[3]
-spotify.search({ type: 'track', query: "sin wagon" }, function (err, data) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
-    }
+// Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
+var axios = require("axios");
 
-    // to check the full response:
-    //console.log(data.tracks.items[0]);
-     
-    // return the artist
-    console.log(data.tracks.items[0].artists[0].name);
-    // return the song's name
-    console.log(data.tracks.items[0].name);
-    // return a preview link of the song from spotify
-    console.log(data.tracks.items[0].preview_url);
-    // return the album that the song is from
-    console.log(data.tracks.items[0].album.name);
-});
+// Takes in all of the command line arguments
+var inputString = process.argv;
+
+// Parses the command line argument to specify the database and search titles
+var database = inputString[2];
+var titles = inputString[3];
+
+//TODO: fix "Sin Wagon" to titles variable
+function runSpotify() {
+    spotify.search({ type: 'track', query: "Sin Wagon" }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        // to check the full response:
+        //console.log(data.tracks.items[0]);
+
+        // return the artist
+        console.log("Artist: " + data.tracks.items[0].artists[0].name);
+        // return the song's name
+        console.log("Song Name: " + data.tracks.items[0].name);
+        // return a preview link of the song from spotify
+        console.log("Preview: " + data.tracks.items[0].preview_url);
+        // return the album that the song is from
+        console.log("Album: " + data.tracks.items[0].album.name);
+    });
+}
+
+//TODO: fix "lion+king" to titles variable
+function runOMDB() {
+    axios.get("http://www.omdbapi.com/?t=lion+king&y=&plot=short&apikey=trilogy").then(
+        function (response) {
+            //console.log(response.data);
+            
+            // return title of the movie
+            console.log("Title: " + response.data.Title);
+            // return year the movie came out. TODO: convert to show year only with moment.js
+            console.log("Released: " + response.data.Released);
+            // return IMDB rating of the movie
+            console.log("IMDB rating: " + response.data.Ratings[0].Value);
+            // return rotten tomatoes rating of the movie
+            console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value);
+            // return country where the movie was produced
+            console.log("Country: " + response.data.Country);
+            // return language of the movie
+            console.log("Language: " + response.data.Language);
+            // return plot of the movie
+            console.log("Plot: " + response.data.Plot);
+            // return actors in the movie
+            console.log("Actors: " + response.data.Actors);
+        }
+    );
+}
+
+// to figure out what category the user wants to search
+//if (database === "concert-this") {
+    //TODO: run the bandsintown function
+//} else if (database === "spotify-this-song") {
+   // runSpotify();
+//} else if (database === "movie-this") {
+    // runOMDB();
+//} else if (database === "do-what-it-says") {
+    //TODO: run do-what-it-says function
+//} else {
+    //console.log("Try typing in one of the following commands before your search term: concert-this, spotify-this-song, movie-this, or do-what-it-says. Thanks!")
+//}
